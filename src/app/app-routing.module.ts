6687a8c8113deae83df3d8e1ccb7c3519dd4f2d4
Namibo06 <namibo654@gmail.com>
2024-03-05 15:09:38 -0300

    create: services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index/index.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { RegisterComponent } from './pages/register/register/register.component';
+import { SuitesComponent } from './pages/suites/suites/suites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:IndexComponent
+  },
+  {
+    path:'iniciar_sessao',
+    component:LoginComponent
+  },
+  {
+    path:'cadastro',
+    component:RegisterComponent
+  },
+  {
+    path:'suite/:id',
+    component:SuitesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header/header.component';
-import { IndexComponent } from './pages/index/index/index.component';
-import { RegisterComponent } from './pages/register/register/register.component';
-import { LoginComponent } from './pages/login/login/login.component';
-import { SuitesComponent } from './pages/suites/suites/suites.component';
+
+
+import { HeaderModule } from './shared/header/header.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { IndexModule } from './pages/index/index.module';
+import { RegisterModule } from './pages/register/register.module';
+import { LoginModule } from './pages/login/login.module';
+import { SuitesModule } from './pages/suites/suites.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    IndexComponent,
-    RegisterComponent,
-    LoginComponent,
-    SuitesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    FooterModule,
+    IndexModule,
+    RegisterModule,
+    LoginModule,
+    SuitesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
